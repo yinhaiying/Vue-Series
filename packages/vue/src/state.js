163923b77aeed95c1afd1e818ddc8ae1e5d10971
yinhaiying <18840842571@163.com>
2021-02-17 07:47:34 +0800

    开始处理data

--- packages/vue/src/state.js
+++ packages/vue/src/state.js
@@ -1,3 +1,5 @@
+import { observe } from "./observer/index.js";
+
 export function initState(vm) {
   const opts = vm.$options;
   if (opts.props) {
@@ -20,7 +22,14 @@ export function initState(vm) {
 
 function initProps(vm) { }
 function initMethods(vm) { }
-function initData(vm) { }
+function initData(vm) {
+  let data = vm.$options.data;
+  data = typeof data === "function" ? data.call(vm) : data;
+  // 数据的劫持
+  // 对象 Object.defineProperty
+  // 数组 对象里面嵌套数组
+  observe(data);
+}
 function initComputed(vm) { }
 function initWatch(vm) { }
 
